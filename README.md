db90a4a68547a64e23b2e33194343d216487282e
Julius Figge <julius@figge.eu>
2022-10-17 07:44:43 +0200

    refactor and introduce manual

---
+++ README.md
@@ -0,0 +1,16 @@
+# video-to-css
+This is a hack just to proof something like this is possible.  
+
+## What the hack is this even?
+You are seeing Video-Playback in CSS only!  
+In order to make this happen, we have to jump trough some hoops together.  
+When clicking the Button, the Videoplayback is started.  
+Concurrently frames are extracted (in reduced resolution) and collected into a List.  
+After the Videoplayback stops (to be changed into async processing), the frames are processed.  
+Processing in this context means extracting the pixels color values and mapping them into CSS box-shadows - yes you heard that correctly!  
+After the frame to css conversion, all frames are collected and mapped into keyframes.  
+To playback the Animation a CSS animation containing the keyframes is added in.
+
+## Requirements
+Currently the functionality of extracting Frames is based on the _MediaStreamTrackProcessor_-API.  
+**Therefore only recent Versions of Chromium based Browsers (>= v94) are supported!**
--- script.js
+++ script.js
@@ -7,32 +7,34 @@ const DESIRED_FRAMERATE = 30;
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    button = document.querySelector('button');
-    canvas = document.querySelector('canvas');
+    button = document.querySelector('#playback-button');
+    canvas = document.querySelector('#playback-canvas');
     ctx = canvas.getContext('2d');
 
     if (!window.MediaStreamTrackProcessor) {
-        console.error("your browser doesn't support this API yet");
+        document.querySelector('#not-supported-warning').style.display = 'unset';
         return;
     }
 
     button.onclick = async () => extractFrames();
 })
 
-function mapPixelsToBoxShadowDeclaration(width, height, pixels) {
-
+const mapPixelsToBoxShadowDeclaration = (width, height, pixels) => {
     const boxShadowPixels = [];
-    [...Array(Math.ceil(pixels.length / 4)).keys()].forEach(i => {
-        const [r, g, b, a] = pixels.slice(i * 4, (i + 1) * 4)
 
-        const x = i % width;
-        const y = Math.floor(i  / width);
+    //extract r,g,b,a values from array -> r1,g1,b1,a1,r2,g2,b2,a2
+    [...Array(Math.ceil(pixels.length / 4)).keys()]
+        .forEach(i => {
+            const [r, g, b, a] = pixels.slice(i * 4, (i + 1) * 4);
 
-        boxShadowPixels.push(`${x}px ${y}px rgb(${r},${g},${b},${a})`);
-    })
+            const x = i % width;
+            const y = Math.floor(i / width);
+
+            boxShadowPixels.push(`${x}px ${y}px rgb(${r},${g},${b},${a})`);
+        })
 
     return boxShadowPixels.join(',') + ';';
-}
+};
 
 const extractPixelsFromFrame = bitmap => {
     const {width: w, height: h} = bitmap;
@@ -42,19 +44,19 @@ const extractPixelsFromFrame = bitmap => {
     ctx.drawImage(bitmap, 0, 0);
     const pixels = ctx.getImageData(0, 0, w, h).data;
 
-    keyFramesList.push(mapPixelsToBoxShadowDeclaration(w,h,pixels));
+    keyFramesList.push(mapPixelsToBoxShadowDeclaration(w, h, pixels));
 };
 
-const readChunk = async (reader) => {
+const readAndProcessFrames = async (reader) => {
     await reader
         .read()
         .then(async ({done, value}) => {
             if (!value) return;
 
+            console.log(value)
 
-            let calculatedWidth = value.codedWidth / VIDEO_SCALING_FACTOR;
-            let calculatedHeight = value.codedHeight / VIDEO_SCALING_FACTOR;
-
+            const calculatedWidth = value.codedWidth / VIDEO_SCALING_FACTOR;
+            const calculatedHeight = value.codedHeight / VIDEO_SCALING_FACTOR;
             const bitmap = await createImageBitmap(value, {
                 resizeWidth: calculatedWidth,
                 resizeHeight: calculatedHeight,
@@ -69,7 +71,7 @@ const readChunk = async (reader) => {
             value.close();
 
             if (done) return;
-            await readChunk(reader);
+            await readAndProcessFrames(reader);
         });
 };
 
@@ -77,15 +79,15 @@ const createKeyFramesDeclaration = (usableAmountOfKeyFrames) => {
     let totalFramesLength = usableAmountOfKeyFrames.length;
 
     usableAmountOfKeyFrames = usableAmountOfKeyFrames.map((element, index) => {
-        let keyFramePercentage = parseFloat(String(index/totalFramesLength)).toFixed(2);
-        return keyFramePercentage+'% { box-shadow: '+element+'}'
+        let keyFramePercentage = parseFloat(String(index / totalFramesLength)).toFixed(2);
+        return keyFramePercentage + '% { box-shadow: ' + element + '}'
     })
 
     const cssMovieKeyframes = document.createElement('style');
-    let keyFramesDeclaration = '@keyframes css-movie {' + usableAmountOfKeyFrames.join(' ')+ '}';
+    let keyFramesDeclaration = '@keyframes css-movie {' + usableAmountOfKeyFrames.join(' ') + '}';
     const rules = document.createTextNode(keyFramesDeclaration);
 
-    cssVideoAnimationLength = (usableAmountOfKeyFrames.length / DESIRED_FRAMERATE)*100;
+    cssVideoAnimationLength = (usableAmountOfKeyFrames.length / DESIRED_FRAMERATE) * 100;
 
     cssMovieKeyframes.appendChild(rules);
     document.getElementsByTagName("head")[0].appendChild(cssMovieKeyframes);
@@ -100,26 +102,10 @@ const animateKeyFrames = () => {
     document.querySelector('.css-video').style.animation = `css-movie ${cssVideoAnimationLength}s steps(1, end)`;
 };
 
-const extractFrames = async () => {
-    const track = await getVideoTrack();
+const setFrameRateFromVideoTrack = track => framerate = track.getSettings().frameRate;
 
-    framerate = track.getSettings().frameRate;
-
-    const processor = new MediaStreamTrackProcessor(track);
-    const reader = processor.readable.getReader();
-
-    await readChunk(reader);
-
-    //TODO reduce framerate beforehands -> this is imperformant as hell
-    console.log(frames.length)
-    const usableAmountOfKeyFrames = reduceFrameRateForKeyFrames();
-    console.log(usableAmountOfKeyFrames.length)
-    createKeyFramesDeclaration(usableAmountOfKeyFrames);
-
-    animateKeyFrames();
-};
-
-async function getVideoTrack() {
+const getVideoTrack = async () => {
+    //TODO introduce video upload?
     const video = document.createElement("video");
     video.crossOrigin = "anonymous";
     video.src = "https://upload.wikimedia.org/wikipedia/commons/e/e4/6-step_example.webm";
@@ -128,5 +114,22 @@ async function getVideoTrack() {
     const [track] = video.captureStream().getVideoTracks();
     video.onended = () => track.stop();
     return track;
-}
+};
+
+const extractFrames = async () => {
+    const track = await getVideoTrack();
+
+    setFrameRateFromVideoTrack(track);
+
+    const processor = new MediaStreamTrackProcessor(track);
+    const reader = processor.readable.getReader();
+
+    await readAndProcessFrames(reader);
+
+    //TODO reduce framerate beforehands -> this is imperformant as hell
+    const usableAmountOfKeyFrames = reduceFrameRateForKeyFrames();
+    createKeyFramesDeclaration(usableAmountOfKeyFrames);
+
+    animateKeyFrames();
+};
 
